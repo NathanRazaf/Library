26639511c984cf5bcba3af84b9866138c694569b
Nathan Razafindrakoto <nathan.razafindrakoto@umontreal.ca>
2023-06-29 20:47:20 -0400

    Add functionality and style to bookForm

--- script.js
+++ script.js
@@ -5,16 +5,60 @@ const library = document.querySelector("div.library");
 const bookForm= document.querySelector("form#bookForm");
 const overlay = document.querySelector("div#overlay");
 const formModal = document.querySelector("div#formModal");
+const closeBtn = document.querySelector("#bookForm .close");
 
 newBookBtn.addEventListener("click", function() {
     overlay.classList.remove("hidden");
     formModal.classList.remove("hidden");
 });
-
 bookForm.addEventListener("submit", function(e) {
     e.preventDefault();
+    addBookToLibrary();
     overlay.classList.add("hidden");
     formModal.classList.add("hidden");
-
-
+    displayBooks();
+    bookForm.reset();
 });
+
+closeBtn.addEventListener("click", function() {
+    overlay.classList.add("hidden");
+    formModal.classList.add("hidden");
+    bookForm.reset();
+})
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = Boolean(read);
+}
+function addBookToLibrary() {
+    let title = bookForm.elements["title"].value;
+    let author = bookForm.elements["author"].value;
+    let pages = bookForm.elements["pages"].value;
+    let read = bookForm.elements["read"].checked;
+    let book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+    console.log(myLibrary);
+}
+function displayBooks() {
+    library.innerHTML= "";
+    myLibrary.forEach(function(book) {
+        let bookCard = document.createElement("div");
+        bookCard.classList.add("bookCard");
+        let title = document.createElement("h2");
+        title.textContent = book.title;
+        let author = document.createElement("h3");
+        author.textContent = book.author;
+        let pages = document.createElement("h3");
+        pages.textContent = book.pages;
+        let read = document.createElement("h3");
+        read.textContent = book.read ? "Read" : "Not Read";
+        read.style.color = book.read ? "green" : "red";
+        bookCard.appendChild(title);
+        bookCard.appendChild(author);
+        bookCard.appendChild(pages);
+        bookCard.appendChild(read);
+        library.appendChild(bookCard);
+    });
+}
